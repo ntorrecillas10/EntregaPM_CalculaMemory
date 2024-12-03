78f61fcf437fe210998d404c72bb274e538623f8
ntorrecillas10 <nalascobic7@gmail.com>
2024-12-03 19:48:22 +0100

    dlsadwa

--- app/src/main/java/com/example/entregapm_calculamemory/Memorytron1.kt
+++ app/src/main/java/com/example/entregapm_calculamemory/Memorytron1.kt
@@ -22,11 +22,10 @@ class Memorytron1 : AppCompatActivity() {
     private lateinit var boton11: AppCompatImageButton
 
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memorytron1)
+
 
         boton0 = findViewById(R.id.imagen00)
         boton1 = findViewById(R.id.imagen01)
@@ -41,8 +40,6 @@ class Memorytron1 : AppCompatActivity() {
         boton10 = findViewById(R.id.imagen31)
         boton11 = findViewById(R.id.imagen32)
 
-        //Guardamos si la carta esta dada la vuelta en un array
-        val cartasVueltas = arrayOf(false, false, false, false, false, false, false, false, false, false, false, false)
         //Guardamos los valores de las cartas en un array
         val cartasValues = arrayOf(0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5)
 
@@ -61,22 +58,254 @@ class Memorytron1 : AppCompatActivity() {
 
         val cartaDefecto = R.drawable.anotaci_n_2024_12_02_180644
         val cartabatman = R.drawable.carta1_batman
+        var cartasEnPulsacion = mutableListOf<Int>()
+        var cartasAcertadas = mutableListOf<Int>()
+
 
         boton0.setOnClickListener {
-            boton0.setImageResource(cartabatman)
-            //Si hay una carta dada la vuelta y coincide con el mismo value, se dejan dadas la vuelta, si no, se dan la vuelta al paso de dos segundos
-            if (cartasVueltas[0] && valorboton0 == valorboton1) {
-                cartasVueltas[0] = true
-                cartasVueltas[1] = true
-            } else {
-                cartasVueltas[0] = false
-                cartasVueltas[1] = false
-                boton0.setImageResource(cartaDefecto)
-                boton1.setImageResource(cartaDefecto)
+            if (cartasEnPulsacion.size == 0) {
+                boton0.setImageResource(R.drawable.carta1_batman)
+                boton0.isEnabled = false
+                cartasEnPulsacion.add(valorboton0)
+            }else {
+                boton0.setImageResource(R.drawable.carta1_batman)
+                boton0.isEnabled = false
+                cartasEnPulsacion.add(valorboton0)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
 
+                } else {
+                    boton0.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton0.isEnabled = true
+                    cartasEnPulsacion.clear()
+                }
             }
         }
+        boton1.setOnClickListener {
+            if (cartasEnPulsacion.size == 0) {
+                boton1.setImageResource(R.drawable.carta1_batman)
+                boton1.isEnabled = false
+                cartasEnPulsacion.add(valorboton1)
+            }else {
+                boton1.setImageResource(R.drawable.carta1_batman)
+                boton1.isEnabled = false
+                cartasEnPulsacion.add(valorboton1)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
 
+                } else {
+                    boton1.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton1.isEnabled = true
+                    cartasEnPulsacion.clear()
+                }
+            }
+        }
+        boton2.setOnClickListener {
+            if (cartasEnPulsacion.size == 0) {
+                boton2.setImageResource(R.drawable.carta1_batman)
+                boton2.isEnabled = false
+                cartasEnPulsacion.add(valorboton2)
+            }else {
+                boton2.setImageResource(R.drawable.carta1_batman)
+                boton2.isEnabled = false
+                cartasEnPulsacion.add(valorboton2)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
 
+                } else {
+                    boton2.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton2.isEnabled = true
+                    cartasEnPulsacion.clear()
+                }
+            }
+        }
+        boton3.setOnClickListener {
+            if (cartasEnPulsacion.size == 0) {
+                boton3.setImageResource(R.drawable.carta1_batman)
+                boton3.isEnabled = false
+                cartasEnPulsacion.add(valorboton3)
+            }else {
+                boton3.setImageResource(R.drawable.carta1_batman)
+                boton3.isEnabled = false
+                cartasEnPulsacion.add(valorboton3)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
+
+                } else {
+                    boton3.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton3.isEnabled = true
+                    cartasEnPulsacion.clear()
+                }
+            }
+        }
+        boton4.setOnClickListener {
+            if (cartasEnPulsacion.size == 0) {
+                boton4.setImageResource(R.drawable.carta1_batman)
+                boton4.isEnabled = false
+                cartasEnPulsacion.add(valorboton4)
+            }else {
+                boton4.setImageResource(R.drawable.carta1_batman)
+                boton4.isEnabled = false
+                cartasEnPulsacion.add(valorboton4)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
+                }else{
+                    boton4.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton4.isEnabled = true
+                    cartasEnPulsacion.clear()
+                }
+            }
+        }
+        boton5.setOnClickListener {
+            if (cartasEnPulsacion.size == 0) {
+                boton5.setImageResource(R.drawable.carta1_batman)
+                boton5.isEnabled = false
+                cartasEnPulsacion.add(valorboton5)
+            } else {
+                boton5.setImageResource(R.drawable.carta1_batman)
+                boton5.isEnabled = false
+                cartasEnPulsacion.add(valorboton5)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
+                } else {
+                    boton5.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton5.isEnabled = true
+                    cartasEnPulsacion.clear()
+                }
+            }
+        }
+        boton6.setOnClickListener {
+            if (cartasEnPulsacion.size == 0) {
+                boton6.setImageResource(R.drawable.carta1_batman)
+                boton6.isEnabled = false
+                cartasEnPulsacion.add(valorboton6)
+            } else {
+                boton6.setImageResource(R.drawable.carta1_batman)
+                boton6.isEnabled = false
+                cartasEnPulsacion.add(valorboton6)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
+                }else{
+                    boton6.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton6.isEnabled = true
+                    cartasEnPulsacion.clear()
+                }
+            }
+        }
+        boton7.setOnClickListener {
+            if (cartasEnPulsacion.size == 0) {
+                boton7.setImageResource(R.drawable.carta1_batman)
+                boton7.isEnabled = false
+                cartasEnPulsacion.add(valorboton7)
+            } else {
+                boton7.setImageResource(R.drawable.carta1_batman)
+                boton7.isEnabled = false
+                cartasEnPulsacion.add(valorboton7)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
+                } else {
+                    boton7.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton7.isEnabled = true
+                    cartasEnPulsacion.clear()
+
+                }
+            }
+        }
+        boton8.setOnClickListener {
+            if (cartasEnPulsacion.size == 0) {
+                boton8.setImageResource(R.drawable.carta1_batman)
+                boton8.isEnabled = false
+                cartasEnPulsacion.add(valorboton8)
+            } else {
+                boton8.setImageResource(R.drawable.carta1_batman)
+                boton8.isEnabled = false
+                cartasEnPulsacion.add(valorboton8)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
+                } else {
+                    boton8.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton8.isEnabled = true
+                    cartasEnPulsacion.clear()
+                }
+            }
+        }
+        boton9.setOnClickListener {
+            if (cartasEnPulsacion.size == 0) {
+                boton9.setImageResource(R.drawable.carta1_batman)
+                boton9.isEnabled = false
+                cartasEnPulsacion.add(valorboton9)
+            } else {
+                boton9.setImageResource(R.drawable.carta1_batman)
+                boton9.isEnabled = false
+                cartasEnPulsacion.add(valorboton9)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
+                } else {
+                    boton9.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton9.isEnabled = true
+                    cartasEnPulsacion.clear()
+                }
+            }
+        }
+        boton10.setOnClickListener {
+            if (cartasEnPulsacion.size == 0) {
+                boton10.setImageResource(R.drawable.carta1_batman)
+                boton10.isEnabled = false
+                cartasEnPulsacion.add(valorboton10)
+            } else {
+                boton10.setImageResource(R.drawable.carta1_batman)
+                boton10.isEnabled = false
+                cartasEnPulsacion.add(valorboton10)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
+            }else{
+                    boton10.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton10.isEnabled = true
+                    cartasEnPulsacion.clear()
+                }
+            }
+        }
+        boton11.setOnClickListener {
+            if (cartasEnPulsacion.size == 0) {
+                boton11.setImageResource(R.drawable.carta1_batman)
+                boton11.isEnabled = false
+                cartasEnPulsacion.add(valorboton11)
+            } else {
+                boton11.setImageResource(R.drawable.carta1_batman)
+                boton11.isEnabled = false
+                cartasEnPulsacion.add(valorboton11)
+                if (cartasEnPulsacion.size == 2 && cartasEnPulsacion[0] == cartasEnPulsacion[1]) {
+                    cartasAcertadas.add(cartasEnPulsacion[0])
+                    cartasAcertadas.add(cartasEnPulsacion[1])
+                    cartasEnPulsacion.clear()
+                }else{
+                    boton11.setImageResource(R.drawable.anotaci_n_2024_12_02_180644)
+                    boton11.isEnabled = true
+                    cartasEnPulsacion.clear()
+                }
+            }
+        }
     }
 }
