1f0731ba1437659e9531647ea714855b35e7ee40
Nacho <nalascobic7@gmail.com>
2024-12-06 21:16:48 +0100

    gybghiuj

--- app/src/main/java/com/example/entregapm_calculamemory/Config_CalculaTron.kt
+++ app/src/main/java/com/example/entregapm_calculamemory/Config_CalculaTron.kt
@@ -1,14 +1,112 @@
 package com.example.entregapm_calculamemory
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.CheckBox
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageButton
+import com.example.entregapm_calculamemory.databinding.ActivityConfigCalculatronBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class Config_CalculaTron : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConfigCalculatronBinding
+    private lateinit var botonVolver: AppCompatImageButton
+    private lateinit var cuentaAtras: TextInputEditText
+    private lateinit var minimo: TextInputEditText
+    private lateinit var maximo: TextInputEditText
+    private lateinit var checkSuma: CheckBox
+    private lateinit var checkResta: CheckBox
+    private lateinit var checkMultiplicacion: CheckBox
+    private lateinit var spinnerAnimaciones: Spinner
+    private lateinit var botonGuardar: AppCompatButton
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_config_calculatron)
+        binding = ActivityConfigCalculatronBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        botonVolver = binding.volver
+        cuentaAtras = binding.cuentaAtras
+        minimo = binding.minimoEdit
+        maximo = binding.maximoEdit
+        checkSuma = binding.checkSuma
+        checkResta = binding.checkResta
+        checkMultiplicacion = binding.checkMultiplicacion
+        spinnerAnimaciones = binding.spinnerAnimaciones
+        botonGuardar = binding.guardar
+        val sharedPref = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE)
+
+
+
+
+        val opciones = arrayOf("Sin animaciones", "Colores graduales", "Colores cambiantes")
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones)
+
+        // Especifica el diseño a usar cuando se despliega la lista.
+                adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Aplica el adaptador al Spinner.
+                val spinner = findViewById<Spinner>(R.id.spinnerAnimaciones)
+                spinner.adapter = adaptador
+
+
+
+
+
+
+
+
+        botonGuardar.setOnClickListener {
+            // Obtener los valores de los campos de texto
+            val cuentaAtrasText = cuentaAtras.text.toString()
+
+            val minimoText = minimo.text.toString()
+
+            val maximoText = maximo.text.toString()
+
+            // Obtener los valores de los checkboxes
+            val sumaChecked = checkSuma.isChecked
+            val restaChecked = checkResta.isChecked
+            val multiplicacionChecked = checkMultiplicacion.isChecked
+
+            // Obtener el valor seleccionado en el Spinner
+            val animacionSeleccionada = spinnerAnimaciones.selectedItem.toString()
+            // Guardar los valores en SharedPreferences
+            val editor = sharedPref.edit()
+            editor.putString("cuentaAtras", cuentaAtrasText)
+            editor.putString("minimo", minimoText)
+            editor.putString("maximo", maximoText)
+            editor.putBoolean("sumaChecked", sumaChecked)
+            editor.putBoolean("restaChecked", restaChecked)
+            editor.putBoolean("multiplicacionChecked", multiplicacionChecked)
+            editor.putString("animacionSeleccionada", animacionSeleccionada)
+            editor.apply()
+
+            val intent = Intent(this, Game_Calculatron::class.java)
+            startActivity(intent)
+
+
+
+
+
+        }
+
+        botonVolver.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
 
     }
+
 }
--- app/src/main/java/com/example/entregapm_calculamemory/Game_Calculatron.kt
+++ app/src/main/java/com/example/entregapm_calculamemory/Game_Calculatron.kt
@@ -1,20 +1,93 @@
 package com.example.entregapm_calculamemory
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.entregapm_calculamemory.databinding.ActivityGameCalculatronBinding
 
 class Game_Calculatron : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGameCalculatronBinding
+    private lateinit var botonvolver: AppCompatImageButton
+    private lateinit var acertadasText: TextView
+    private lateinit var falladasText: TextView
+    private lateinit var cuentaAtrasText: TextView
+    private lateinit var resultadoAnterior: TextView
+    private lateinit var operacionActual: TextView
+    private lateinit var resultadoSiguiente: TextView
+    private lateinit var botonnum1: AppCompatButton
+    private lateinit var botonnum2: AppCompatButton
+    private lateinit var botonnum3: AppCompatButton
+    private lateinit var botonnum4: AppCompatButton
+    private lateinit var botonnum5: AppCompatButton
+    private lateinit var botonnum6: AppCompatButton
+    private lateinit var botonnum7: AppCompatButton
+    private lateinit var botonnum8: AppCompatButton
+    private lateinit var botonnum9: AppCompatButton
+    private lateinit var botonnum0: AppCompatButton
+    private lateinit var botonReiniciarOperacion: AppCompatButton
+    private lateinit var botonEliminar: AppCompatButton
+    private lateinit var botonIgual: AppCompatButton
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_game_calculatron)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityGameCalculatronBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        botonvolver = binding.volver
+        acertadasText = binding.acertadasCuenta
+        falladasText = binding.falladasCuenta
+        cuentaAtrasText = binding.cuentaAtrasgc
+        resultadoAnterior = binding.resultadoAnterior
+        operacionActual = binding.operacionActual
+        resultadoSiguiente = binding.resultadoSiguiente
+        botonnum1 = binding.botonNum1
+        botonnum2 = binding.botonNum2
+        botonnum3 = binding.botonNum3
+        botonnum4 = binding.botonNum4
+        botonnum5 = binding.botonNum5
+        botonnum6 = binding.botonNum6
+        botonnum7 = binding.botonNum7
+        botonnum8 = binding.botonNum8
+        botonnum9 = binding.botonNum9
+        botonnum0 = binding.botonNum0
+        botonReiniciarOperacion = binding.botonReinicarOperacion
+        botonEliminar = binding.botonEliminarUltimoDigito
+        botonIgual = binding.botonIgual
+
+
+
+        val sharedPref = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE)
+        val cuentaAtras = sharedPref.getString("cuentaAtras", "")
+        val minimo = sharedPref.getString("minimo", "")
+        val maximo = sharedPref.getString("maximo", "")
+//        val sumaCheck = sharedPref.getString("sumaChecked", "")
+//        val restaCheck = sharedPref.getString("restaChecked", "")
+//        val multiplicacionCheck = sharedPref.getString("multiplicacionChecked", "")
+        val animacionSeleccionada = sharedPref.getString("animacionSeleccionada", "")
+
+        //La información recibida en cuentaAtras pasa a ser el numero de cuenta atras
+        cuentaAtras.toString()
+        cuentaAtrasText.text = cuentaAtras
+
+
+
+        botonvolver.setOnClickListener {
+            val intent = Intent(this, Config_CalculaTron::class.java)
+            startActivity(intent)
         }
+
+
     }
 }
